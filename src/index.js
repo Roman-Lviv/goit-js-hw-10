0067807938e9753549b171abe19af844a3d08ac5
Roman-Lviv <kituraroman@gmail.com>
2023-10-22 13:43:56 +0300

    git add .

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import SlimSelect from 'slim-select';
 import 'slim-select/dist/slimselect.css';
-import { fetchBreeds, fetchCatByBreed } from './cat-api'; 
+
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
 
 const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
