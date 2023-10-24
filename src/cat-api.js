51062daa0586714df18ccce19714edfadc067598
Roman-Lviv <kituraroman@gmail.com>
2023-10-24 20:08:51 +0300

    git add .

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,18 +3,13 @@ import axios from 'axios';
 axios.defaults.headers.common['x-api-key'] =
   'live_NhSrRxtwZj7uypmgbjo4Tmh4DCoEpAzSYngICE3X5fge2sZZWwQja8SMmW7HlaJe';
 
+const BASE_URL = 'https://api.thecatapi.com/v1';
 const fetchBreeds = () => {
-  return axios
-    .get('https://api.thecatapi.com/v1/breeds')
-    .then(response => response.data);
+  return axios.get('${BASE_URL}/breeds').then(response => response.data);
 };
 
 const fetchCatByBreed = breedId => {
-  return axios
-    .get(
-      'live_NhSrRxtwZj7uypmgbjo4Tmh4DCoEpAzSYngICE3X5fge2sZZWwQja8SMmW7HlaJe'
-    )
-    .then(response => response.data);
+  return axios.get('${BASE_URL}/Image/search/').then(response => response.data);
 };
 
 export { fetchBreeds, fetchCatByBreed };
