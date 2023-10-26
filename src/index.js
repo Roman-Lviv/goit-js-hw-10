6dc43731f92a717fba2c3934a8143e7b0a5c7ac2
Roman-Lviv <kituraroman@gmail.com>
2023-10-26 21:23:34 +0300

    git add .

--- src/index.js
+++ src/index.js
@@ -6,67 +6,34 @@ const breedSelect = document.querySelector('.breed-select');
 const loader = document.querySelector('.loader');
 const error = document.querySelector('.error');
 const catInfo = document.querySelector('.cat-info');
+
+breedSelect.addEventListener('change', onChange);
 let select;
 
 select = new SlimSelect({
   select: '.breed-select',
-  placeholder: 'Loading breeds...',
+
+  settings: {
+    placeholderText: 'Loading breeds...',
+  },
 });
 
-const populateBreeds = () => {
-  fetchBreeds()
-    .then(res => {
-      const options = res.map(breed => ({
-        text: breed.name,
-        value: breed.id,
-      }));
-      select.setData(options);
-      breedSelect.classList.remove('is-hidden');
-      loader.style.display = 'none';
-    })
-    .catch(err => {
-      loader.style.display = 'none';
-      error.style.display = 'block';
-      console.error('Error fetching breeds:', err);
-    });
-};
+fetchBreeds()
+  .then(res => {
+    const options = res.map(breed => ({
+      text: breed.name,
+      value: breed.id,
+    }));
+    select.setData(options);
+    breedSelect.classList.remove('is-hidden');
+    loader.style.display = 'none';
+  })
+  .catch(err => {
+    loader.style.display = 'none';
+    error.style.display = 'block';
+    console.error('Error fetching breeds:', err);
+  });
 
-const fetchCatInfo = breedId => {
-  loader.style.display = 'block';
-  catInfo.innerHTML = '';
-  error.style.display = 'none';
-
-  fetchCatByBreed(breedId)
-    .then(catData => {
-      const catImage = document.createElement('img');
-      catImage.src = catData.url;
-
-      const catName = document.createElement('p');
-      catName.textContent = `Breed: ${catData.breeds[0].name}`;
-
-      const catDescription = document.createElement('p');
-      catDescription.textContent = `Description: ${catData.breeds[0].description}`;
-
-      const catTemperament = document.createElement('p');
-      catTemperament.textContent = `Temperament: ${catData.breeds[0].temperament}`;
-
-      catInfo.appendChild(catImage);
-      catInfo.appendChild(catName);
-      catInfo.appendChild(catDescription);
-      catInfo.appendChild(catTemperament);
-
-      loader.style.display = 'none';
-    })
-    .catch(err => {
-      loader.style.display = 'none';
-      error.style.display = 'block';
-      console.error('Error fetching cat info:', err);
-    });
-};
-
-breedSelect.addEventListener('change', event => {
-  const selectedBreedId = event.target.value;
-  fetchCatInfo(selectedBreedId);
-});
-
-populateBreeds();
+function onChange(e) {
+  fetchCatByBreed(e.target.value).then(console.log);
+}
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
+
 axios.defaults.headers.common['x-api-key'] =
   'live_NhSrRxtwZj7uypmgbjo4Tmh4DCoEpAzSYngICE3X5fge2sZZWwQja8SMmW7HlaJe';
+
 const BASE_URL = 'https://api.thecatapi.com/v1';
+
 const fetchBreeds = () => {
   return axios.get(`${BASE_URL}/breeds`).then(response => response.data);
 };
+
 const fetchCatByBreed = breedId => {
-  return axios.get(`${BASE_URL}/Image/search/`).then(response => response.data);
+  return axios
+    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+    .then(response => response.data);
 };
+
 export { fetchBreeds, fetchCatByBreed };
